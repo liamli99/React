8dbd303f52184073e5efd8a7d73f3c79601c3cd1
LiamLi <zl127@illinois.edu>
2024-08-02 22:09:53 -0500

    08-lorem-ipsum

--- 04-advanced-projects/08-lorem-ipsum/src/App.jsx
+++ 04-advanced-projects/08-lorem-ipsum/src/App.jsx
@@ -1,8 +1,38 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import data from './data';
 
 function App() {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Input value is string!!!
+    const amount = Number(count);
+    setText(data.slice(0, amount));
+  }
+
   return (
-    <h1>hello</h1>
+    <section className='section-center'>
+      <h4>Lorem Ipsum</h4>
+      
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>Paragraphs:</label>
+        <input type='number' min='1' max='8' id='amount' value={count} onChange={(e) => setCount(e.target.value)} />
+        <button className='btn'>Generate</button>
+      </form>
+
+      <article className='lorem-text'>
+        {text.map((item) => {
+          return (
+            <p key={nanoid()}>{item}</p>
+          );
+        })}
+      </article>
+    </section>
   );
 }
 
