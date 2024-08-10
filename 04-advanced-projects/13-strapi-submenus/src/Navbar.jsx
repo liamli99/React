b650d02afbb2fc0b4076983cedf7add30631dc3a
LiamLi <zl127@illinois.edu>
2024-08-10 11:57:26 -0500

    finish submenu

--- 04-advanced-projects/13-strapi-submenus/src/Navbar.jsx
+++ 04-advanced-projects/13-strapi-submenus/src/Navbar.jsx
@@ -3,10 +3,19 @@ import { useAppContext } from './AppContext';
 import NavLinks from './NavLinks';
 
 const Navbar = () => {
-  const { openSidebar } = useAppContext();
+  const { openSidebar, setPageId } = useAppContext();
+  
+  // onMouseOver: triggers when the mouse enters the element and its children
+  // onMouseEnter: triggers when the mouse enters the element
+  // https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_event_mouseenter_mouseover
+  const handleMouseOver = (e) => {
+    if (!e.target.classList.contains('nav-link')) {
+      setPageId(null);
+    }
+  }
 
   return (
-    <nav>
+    <nav onMouseOver={handleMouseOver}>
       <div className='nav-center'>
         <h3 className='logo'>Strapi</h3>
         
